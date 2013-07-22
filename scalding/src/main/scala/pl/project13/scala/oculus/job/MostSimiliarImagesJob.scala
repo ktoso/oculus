a58197b031201a30c9bd1c2ade2977cc014ff8a1
Konrad Malawski <konrad.malawski@project13.pl>
2013-07-23 00:45:42 +0100

    initial commit of template

---
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/MostSimiliarImagesJob.scala
@@ -0,0 +1,21 @@
+package pl.project13.scala.oculus.job
+
+import com.twitter.scalding._
+
+class MostSimiliarImagesJob(args: Args) extends Job(args) {
+
+  val inputFile = args("input")
+  val outputFile = args("output")
+
+  TextLine(inputFile)
+    .flatMap('line -> 'word) { line: String => tokenize(line) }
+    .groupBy('word) { _.size }
+    .write(Tsv(outputFile))
+
+
+  def tokenize(text: String): Array[String] = {
+    // Lowercase each word and remove punctuation.
+    text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
+  }
+
+}
