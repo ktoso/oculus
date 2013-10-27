30451c9c618875f6288e3be39125b4ed749e6f58
Konrad Malawski <konrad.malawski@project13.pl>
2013-10-27 01:55:26 +0000

    lo

--- downloader/src/main/scala/pl/project13/scala/oculus/hbase/HBaseInit.scala
+++ downloader/src/main/scala/pl/project13/scala/oculus/hbase/HBaseInit.scala
@@ -1,16 +1,12 @@
 package pl.project13.scala.oculus.hbase
 
 import pl.project13.scala.oculus.hbase.tables.MetadataSchema
+import scala.Predef._
 
 object HBaseInit {
   
   def init() {
-    println(
-      s"""
-        |Prepare HBase with the following tables:
-        |
-        |${MetadataSchema.MetadataTable.createScript()}
-        |
-      """.stripMargin)
+    println("Prepare HBase with the following tables:")
+    println(MetadataSchema.MetadataTable.createScript())
   }
 }
