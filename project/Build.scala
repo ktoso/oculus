219f78c5ef7deca6e40b8fc05a58eb4334a31def
Konrad Malawski <konrad.malawski@project13.pl>
2013-10-27 02:29:02 +0000

    ifix

--- project/Build.scala
+++ project/Build.scala
@@ -78,9 +78,7 @@ object Dependencies {
   lazy val hadoopClient = "org.apache.hadoop" % "hadoop-client" % "1.1.2"
 
   // hbase
-//  lazy val hPaste     = "com.gravity"       % "gravity-hpaste"      % "0.1.11" from "http://up.project13.pl/files/gravity-hpaste-0.1.24-SNAPSHOT.jar"
-  lazy val hPaste     = "com.gravity"       % "gravity-hpaste"      % "0.1.11" from "file:///Users/kmalawski/.m2/repository/com/gravity/gravity-hpaste/0.1.24-SNAPSHOT/gravity-hpaste-0.1.24-SNAPSHOT.jar"
-//  lazy val hbase      = "org.apache.hbase"  % "hbase"               % "0.94.6-cdh4.3.1"
+  lazy val hPaste     = "com.gravity"       % "gravity-hpaste"      % "0.1.11"
   lazy val hbase      = "org.apache.hbase"  % "hbase"               % "0.94.6.1"
 
   lazy val hadoops = Seq(hadoopCore, hadoopClient, hbase)
