105fe4fd2360a1402a2432dd73f249e947648f07
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-19 02:02:49 +0000

    pushing

--- downloader/src/main/scala/pl/project13/scala/oculus/actor/HDFSUploadActor.scala
+++ downloader/src/main/scala/pl/project13/scala/oculus/actor/HDFSUploadActor.scala
@@ -6,29 +6,17 @@ import pl.project13.scala.oculus.hdfs.{HBaseActions, HDFSActions}
 import pl.project13.scala.oculus.ffmpeg.FFMPEG
 import java.io.File
 import pl.project13.scala.rainbow._
-import akka.pattern.AskSupport
-import scala.concurrent.{Future, Await}
-import akka.util.Timeout
-import scala.concurrent.duration._
 
 class HDFSUploadActor(hdfsLocation: String) extends Actor with ActorLogging
-  with AskSupport
   with HDFSActions
   with HBaseActions {
 
-  implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
-
-  implicit val timeout = Timeout(5 minutes)
-
   def receive = {
     case RequestUploadToHDFS(local: DownloadedVideoFile) =>
-      val me = self
-      val f1 = me ? UploadFileToHDFS(local)
-      val f2 = me ? UploadAsSequenceFileToHDFS(local)
+//      not required to store entire video
+//      self ! UploadAsSequenceFileToHDFS(local) // and await
+      self ! UploadAsSequenceFileToHDFS(local)
 
-      Future.sequence(List(f1, f2)) onComplete { f =>
-        me ! CleanupAfterUploads(local)
-      }
 
     case UploadAsSequenceFileToHDFS(local: DownloadedVideoFile) if local.file.exists =>
       log.info("Got request to upload as sequence file [%s] to HDFS...".format(local.fullName))
@@ -41,6 +29,7 @@ class HDFSUploadActor(hdfsLocation: String) extends Actor with ActorLogging
     case UploadAsSequenceFileToHDFS(local: DownloadedVideoFile) if !local.file.exists =>
       log.warning("Got request to upload not existing file[%s] to HDFS! Ignoring...".format(local.fullName))
 
+      
     case UploadFileToHDFS(local: DownloadedVideoFile) if local.file.exists =>
       log.info("Will upload plain file [%s] to HDFS...".format(local.fullName))
       upload(local.file, createTargetPath(local), delSrc = true)
