36f6cf3032037afed3bc94ed3f8bee110bc330d5
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-20 00:10:29 +0000

    implemented decoding MH hashing

---
+++ common/src/main/scala/pl/project13/scala/oculus/phash/PHash.scala
@@ -0,0 +1,75 @@
+package pl.project13.scala.oculus.phash
+
+import java.io.File
+
+import scala.sys.process._
+
+object PHash {
+
+  val MhPrefix = "mh_hash: "
+  val DctPrefix = "dct_hash: "
+  val Zero = "0"
+  val ZeroByte = Zero.getBytes.head
+
+  object MH {
+    def decode(b: String): Array[Int] = {
+      val noLeadingZeros = b.dropWhile(_ == ZeroByte)
+      noLeadingZeros.sliding(2, 2).map(a => Integer.parseInt(a, 16)).toArray
+    }
+  }
+  object DCT {
+    def decode(s: String): String = {
+      s
+    }
+  }
+
+  case class PHashResult(mhHash: String, dctHash: String)
+
+  def analyzeImage(f: File): PHashResult = {
+    val cmnd = List("image_hashes", f.getAbsoluteFile) mkString " "
+
+    println("Exec: " + cmnd)
+    val output = Process(cmnd).!!
+
+    parse(output)
+  }
+
+  private[phash] def parse(s: String): PHashResult = {
+    val lines = s.split("\n")
+    val mh = lines.find(_ contains MhPrefix) map { _.replace(MhPrefix, "") } getOrElse { throw new RuntimeException(s"Unable to find mh hash, in: [$s]") }
+    val dct = lines.find(_ contains DctPrefix) map { _.replace(DctPrefix, "") } getOrElse { throw new RuntimeException(s"Unable to find dct hash, in: [$s]") }
+
+    println("mh = " + mh)
+    println("dct = " + dct)
+
+    PHashResult(
+      mhHash = mh.split(" ").map(a => padLeft(a.toInt.toHexString, 2, Zero)).mkString,
+      dctHash = dct
+    )
+  }
+
+  private def zeroPadded(s: Array[String], width: Int, b: String): Array[String] = {
+    val l = s.length
+    if (l >= width){
+      s
+    } else {
+      val a = new Array[String](width)
+      var i = 0
+      val until = width - l
+      while (i < until) {
+        a(i) = b
+        i += 1
+      }
+      System.arraycopy(s, 0, a, i, l)
+
+      a
+    }
+  }
+
+  private def padLeft(s: String, width: Int, ch: String) = {
+    val l = s.length
+    val left = width - l
+    (ch * left) + s
+  }
+
+}
--- downloader/src/test/scala/pl/project13/scala/oculus/phash/PHashTest.scala
+++ downloader/src/test/scala/pl/project13/scala/oculus/phash/PHashTest.scala
@@ -8,6 +8,9 @@ class PHashTest extends FlatSpec with ShouldMatchers {
 
   behavior of "PHash"
 
+  var mhHash: String = _
+  var dctHash: String = _
+
   it should "parse output" in {
     // given
     val consoleOutput =
@@ -17,11 +20,35 @@ class PHashTest extends FlatSpec with ShouldMatchers {
     // when
     val PHashResult(mhHash, dctHash) = PHash.parse(consoleOutput)
 
-    info("mhHash = " + mhHash.mkString)
+    this.mhHash = mhHash
+    this.dctHash = dctHash
+
+    info("mhHash  = " + mhHash.mkString)
     info("dctHash = " + dctHash.mkString)
 
+    // no assert here
+  }
+
+  it should "can decode MH encoded hashes (from hbase keys)" in {
+    // when
+    val got = PHash.MH.decode(mhHash)
+    info("decoded mh = " + got.mkString(" "))
+
     // then
-    mhHash.length should be >= 80
-    dctHash.length should be >= 80
+    val expected = Array(66, 248, 221, 150, 127, 29, 1, 183, 4, 203, 178, 146, 205, 127, 64, 224, 143, 117, 72, 166, 211, 100, 115, 233, 203, 98, 39, 30, 121, 185, 216, 50, 110, 72, 141, 172, 120, 148, 102, 1)
+    got.length should equal (expected.length)
+    got should equal (expected)
+  }
+
+  // in case I decide to encode it somehow
+  it should "can decode DCT encoded hashes (from hbase keys)" in {
+    // when
+    val got = PHash.DCT.decode(dctHash)
+    info("decoded dct = " + got.mkString(" "))
+
+    // then
+    val expected = Array(66, 248, 221, 150, 127, 29, 1, 183, 4, 203, 178, 146, 205, 127, 64, 224, 143, 117, 72, 166, 211, 100, 115, 233, 203, 98, 39, 30, 121, 185, 216, 50, 110, 72, 141, 172, 120, 148, 102, 1)
+    got.length should equal (expected.length)
+    got should equal (expected)
   }
 }
