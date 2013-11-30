5048c4064ffb86055b390169e5200dabd6b46e31
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-30 22:58:21 +0000

    fix

--- scalding/src/main/scala/pl/project13/scala/oculus/JobRunner.scala
+++ scalding/src/main/scala/pl/project13/scala/oculus/JobRunner.scala
@@ -33,14 +33,45 @@ object JobRunner extends App with OculusJobs {
 }
 
 trait OculusJobs {
+  import pl.project13.scala.rainbow._
+
+  private val conf = new Configuration
+  private val tool = new scalding.Tool
+
+  /** Override if you need other than default settings - loads up ''application.conf'' */
+  def appConfig: Config = ConfigFactory.load()
+
+  conf.setStrings("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
+  allOculusHadoopSettings(appConfig) foreach { case (key, value) =>
+    conf.setStrings(key, value.unwrapped.toString)
+  }
+
+
   def hashAllSequenceFiles() = {
     val seqFiles = FileUtil.list(new File("/oculus/source/")) filter { _.endsWith(".seq") }
 
-    println(s"Found [${seqFiles.length}] sequence files. Will hash all of them.")
+    println(s"Found [${seqFiles.length}] sequence files. Will hash all of them.".green)
 
-    for(seq <- seqFiles)
-      ScaldingJobRunner.main(Array(
-        "--input", seq
-      ))
+    for(seq <- seqFiles) {
+      val allArgs = Array(
+        classOf[HashVideoSeqFilesJob].getCanonicalName,
+        "--input", seq,
+        "--hdfs", IPs.HadoopMasterWithPort
+      )
+
+      println("-----------------------------------")
+      println("allArgs = " + allArgs.toList)
+      println("-----------------------------------")
+
+      ToolRunner.run(conf, tool, allArgs)
+      println(s"Finished running scalding job for [$seq}]".green)
+    }
   }
+
+
+
+
+
+    private def allOculusHadoopSettings(configuration: com.typesafe.config.Config) =
+      configuration.getConfig("oculus").getConfig("hadoop").entrySet().toList.map(a => (a.getKey, a.getValue))
 }
