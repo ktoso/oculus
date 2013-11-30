8737f410f41bc015d44c52912534a0b1267e190f
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-30 21:45:37 +0000

    fix

--- downloader/src/main/scala/pl/project13/scala/oculus/PreppedMovieUrls.scala
+++ downloader/src/main/scala/pl/project13/scala/oculus/PreppedMovieUrls.scala
@@ -2,12 +2,15 @@ package pl.project13.scala.oculus
 
 object PreppedMovieUrls {
 
+  val watch = "http://www.youtube.com/watch?v="
+  
   object Original {
-    val AttackOnTitan = "http://www.youtube.com/watch?v=XMXgHfHxKVM"
+    val AttackOnTitan = watch + " XMXgHfHxKVM"
+    val VocaloidIARevengeSyndrome  = watch + "z9_QQIWNovc"
   }
 
   object Mirrored {
-    val AttackOnTitan = "http://www.youtube.com/watch?v=HTYBXw-RlzM"
+    val AttackOnTitan = watch + "HTYBXw-RlzM"
   }
 
 }
--- downloader/src/test/scala/pl/project13/scala/oculus/distance/DistanceTest.scala
+++ downloader/src/test/scala/pl/project13/scala/oculus/distance/DistanceTest.scala
@@ -2,6 +2,7 @@ package pl.project13.scala.oculus.distance
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import com.google.common.base.Charsets
 
 class DistanceTest extends FlatSpec with ShouldMatchers {
 
@@ -19,6 +20,23 @@ class DistanceTest extends FlatSpec with ShouldMatchers {
 
     // then
     dist should equal(6)
+  }
+
+  it should "calculate distance between two known hashes" in {
+    // given
+    val b1   = "f8a1d2b1bc083c92da4e82786416d8b725141aa4d48d254c6127228c8e8ecc91ea3562144d075dc6db4897a5a46a61701486d1b18b0bf20e7249a6e74b13205b0f966a19044a2540".getBytes(Charsets.UTF_8)
+    val near = "f8b589454f97132b5cff7170900c0003fcaea7e5000000000163e2f6fec00000000c6000d5c8bfc04005dc3e047c8ef3938be7693dc2c426366c5e4b1c95a5f7caaa3176780f4b55".getBytes(Charsets.UTF_8)
+    val far  = "0085925b2b1ecd4a04255719ba6ee2d8d8edc9fd2db258dda2039401eead9667a4893c76d329494d33697163ac6ce972c94f100df12263cb12cec4397870e8dba4ae74dd030f87cf".getBytes(Charsets.UTF_8)
+
+    // when
+    val distNear = hammingDistance(b1, near)
+    val distFar = hammingDistance(b1, far)
+
+    // then
+    info("dist far  = " + distNear)
+    info("dist near = " + distFar)
+
+    distNear should be < distFar
   }
 
 }
