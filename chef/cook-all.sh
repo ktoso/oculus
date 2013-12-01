dbaa727c58bb81f282e9efea00d1ee688cf86984
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-01 22:54:36 +0000

    cooking all nodes

--- chef/cook-all.sh
+++ chef/cook-all.sh
@@ -1,4 +1,8 @@
-knife solo cook ktoso@108.59.81.83 nodes/master.json
+host='ktoso@108.59.81.83'
+knife solo prepare $host
+knife solo cook $host nodes/master.json
 
 # slaves
-knife solo cook ktoso@23.236.57.71 nodes/master.json
+host='kmalawski@23.236.57.71'
+knife solo prepare $host
+knife solo cook $host nodes/master.json
