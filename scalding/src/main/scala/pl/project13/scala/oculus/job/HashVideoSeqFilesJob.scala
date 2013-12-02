223c144866a16ed191cfb5ce17a144f1f7708d4e
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-02 01:19:56 +0000

    fix

--- scalding/src/main/scala/pl/project13/scala/oculus/job/HashVideoSeqFilesJob.scala
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/HashVideoSeqFilesJob.scala
@@ -18,15 +18,15 @@ class HashVideoSeqFilesJob(args: Args) extends Job(args)
     quorumNames = IPs.HadoopMasterWithPort,
     keyFields = 'mhHash,
     familyNames = Array("youtube"),
-    valueFields = Array('id, 'frame)
+    valueFields = Array('id)
   )
 
   override val youtubeId = FilenameUtils.getBaseName(input)
 
   WritableSequenceFile(input, ('key, 'value))
     .read
-    .mapTo(('key, 'value) -> ('frame, 'mhHash)) { p: SeqFileElement => p._1 -> mhHash(p) }
-    .insert('id, youtubeId)
+    .mapTo(('key, 'value) -> 'mhHash) { p: SeqFileElement => mhHash(p) }
+    .insert('id, youtubeId.asImmutableBytesWriteable)
     .write(Hashes)
 
 }
