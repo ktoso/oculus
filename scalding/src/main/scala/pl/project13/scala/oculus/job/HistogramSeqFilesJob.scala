c408886ca1215e5bc858545e56cfa9f825f9116d
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-02 01:56:03 +0000

    fix

---
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/HistogramSeqFilesJob.scala
@@ -0,0 +1,34 @@
+package pl.project13.scala.oculus.job
+
+import com.twitter.scalding._
+import pl.project13.scala.oculus.IPs
+import pl.project13.scala.scalding.hbase.MyHBaseSource
+import org.apache.commons.io.FilenameUtils
+
+class HistogramSeqFilesJob(args: Args) extends Job(args)
+  with PHashing {
+
+  val input = args("input")
+
+  implicit val mode = Read
+
+  val Hashes = new MyHBaseSource(
+    tableName = "histograms",
+    quorumNames = IPs.HadoopMasterWithPort,
+    keyFields = 'histogram,
+    familyNames = Array("youtube"),
+    valueFields = Array('id, 'frame)
+  )
+
+  override val youtubeId = FilenameUtils.getBaseName(input)
+
+  WritableSequenceFile(input, ('key, 'value))
+    .read
+    .rename('key, 'frame)
+    .map(('frame, 'value) -> ('id, 'mhHash)) { p: SeqFileElement =>
+      youtubeId.asImmutableBytesWriteable -> mhHash(p)
+    }
+    .write(Hashes)
+
+}
+
