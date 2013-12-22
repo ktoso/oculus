25154e520469b6cc4ceb2fcfbde86e67012e0885
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-22 02:52:15 +0100

    Revert "trying to force dist mode"
    This reverts commit 9c449409ca86c7102f77fdc07df78d2bbef160a7.

--- common/src/main/scala/pl/project13/scala/oculus/IPs.scala
+++ common/src/main/scala/pl/project13/scala/oculus/IPs.scala
@@ -2,7 +2,7 @@ package pl.project13.scala.oculus
 
 object IPs {
 
-  val HadoopMaster = "108.59.81.83"
+  val HadoopMaster = "10.240.57.179"
   val HadoopMasterWithPort = HadoopMaster + ":9000"
 
 }
