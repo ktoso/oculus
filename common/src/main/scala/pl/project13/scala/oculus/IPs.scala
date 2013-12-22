9c449409ca86c7102f77fdc07df78d2bbef160a7
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-22 02:47:22 +0100

    trying to force dist mode

--- common/src/main/scala/pl/project13/scala/oculus/IPs.scala
+++ common/src/main/scala/pl/project13/scala/oculus/IPs.scala
@@ -2,7 +2,7 @@ package pl.project13.scala.oculus
 
 object IPs {
 
-  val HadoopMaster = "10.240.57.179"
+  val HadoopMaster = "108.59.81.83"
   val HadoopMasterWithPort = HadoopMaster + ":9000"
 
 }
