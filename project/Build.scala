48cb1b84234c8a5df4b176e1a536686d1bf6fda1
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-22 02:56:22 +0100

    build update

--- project/Build.scala
+++ project/Build.scala
@@ -140,9 +140,44 @@ object Dependencies {
 
 }
 
+object ScaldingAssembly {
+  import sbtassembly.Plugin._
+  import AssemblyKeys._
+
+  lazy val sbtAssemblySettings = assemblySettings ++ Seq(
+
+    // Slightly cleaner jar name
+    jarName in assembly <<= (name, version) { (name, version) => name + "-" + version + ".jar" },
+
+    // Drop these jars
+//    excludedJars in assembly <<= (fullClasspath in assembly) map { cp =>
+//      val excludes = Set(
+//        "jsp-api-2.1-6.1.14.jar",
+//        "jsp-2.1-6.1.14.jar",
+//        "jasper-compiler-5.5.12.jar",
+//        "minlog-1.2.jar", // Otherwise causes conflicts with Kyro (which bundles it)
+//        "janino-2.5.16.jar", // Janino includes a broken signature, and is not needed anyway
+//        "commons-beanutils-core-1.8.0.jar", // Clash with each other and with commons-collections
+//        "commons-beanutils-1.7.0.jar",      // "
+//        "hadoop-core-0.20.2.jar", // Provided by Amazon EMR. Delete this line if you're not on EMR
+//        "hadoop-tools-0.20.2.jar" // "
+//      )
+//      cp filter { jar => excludes(jar.data.getName) }
+//    },
+
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+      (old) => {
+        case "project.clj" => MergeStrategy.discard // Leiningen build files
+        case x => old(x)
+      }
+    }
+  )
+}
+
 object OculusBuild extends Build {
   import BuildSettings._
   import Dependencies._
+  import ScaldingAssembly._
 
   lazy val root = Project("root", file("."), settings = buildSettings)
     .settings(
@@ -162,7 +197,7 @@ object OculusBuild extends Build {
   lazy val scalding = Project(
     "scalding",
     file("scalding"),
-    settings = buildSettings ++ assemblySettings ++
+    settings = buildSettings ++ assemblySettings ++ sbtAssemblySettings ++
       Seq(
         libraryDependencies ++= hadoops ++ scaldingAll ++ akka2Full ++ testing ++ Seq(hPaste),
         mainClass in assembly := Some("pl.project13.scala.oculus.ScaldingJobRunner")
