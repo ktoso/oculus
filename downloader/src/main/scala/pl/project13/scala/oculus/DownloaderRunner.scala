4665d8280c9147597a0d27635b32dd73dc465694
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-24 02:23:33 +0100

    fix

--- downloader/src/main/scala/pl/project13/scala/oculus/DownloaderRunner.scala
+++ downloader/src/main/scala/pl/project13/scala/oculus/DownloaderRunner.scala
@@ -24,12 +24,13 @@ object DownloaderRunner extends App {
   // download specific videos
 //  youtubeDownloader ! DownloadFromYoutube("http://www.youtube.com/watch?v=fFK_YCS8ab0&list=WLFDF76FA8065675A9", crawl = false)
 
-  youtubeDownloader ! DownloadFromYoutube(PreppedMovieUrls.Mirrored.AttackOnTitan, crawl = true)
-  youtubeDownloader ! DownloadFromYoutube(PreppedMovieUrls.Original.AttackOnTitan, crawl = true)
-
-  youtubeCrawler ! CrawlYoutubePage(PreppedMovieUrls.Original.AttackOnTitan)
+//  youtubeDownloader ! DownloadFromYoutube(PreppedMovieUrls.Mirrored.AttackOnTitan, crawl = true)
+//  youtubeDownloader ! DownloadFromYoutube(PreppedMovieUrls.Original.AttackOnTitan, crawl = true)
+//
+//  youtubeCrawler ! CrawlYoutubePage(PreppedMovieUrls.Original.AttackOnTitan)
 //  youtubeCrawler ! CrawlYoutubePage(PreppedMovieUrls.Original.VocaloidIARevengeSyndrome)
-  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=g8evyE9TuYk") // batman
+//  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=g8evyE9TuYk") // batman
+  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=-fcGzCbnTQE") // top anime openings, includes titan (few seconds)
 //  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=fFK_YCS8ab0&list=WLFDF76FA8065675A9") // vocaloids
 //  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=uAcD7H53220") // spiderman
 //  youtubeCrawler ! CrawlYoutubePage("http://www.youtube.com/watch?v=GAkw_Wi4yIo") // tchaikovsky
