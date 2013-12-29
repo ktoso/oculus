3026a8e74e084f9ff60ec209869cdd1570b9685e
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-29 22:02:29 +0100

    new nodes

--- chef/cook-all.sh
+++ chef/cook-all.sh
@@ -8,3 +8,11 @@ knife solo cook $host nodes/master.json
 host='kmalawski@23.236.57.222'
 knife solo prepare $host
 knife solo cook $host nodes/master.json
+
+host='kmalawski@108.59.86.163'
+knife solo prepare $host
+knife solo cook $host nodes/master.json
+
+host='kmalawski@162.222.176.34'
+knife solo prepare $host
+knife solo cook $host nodes/master.json
