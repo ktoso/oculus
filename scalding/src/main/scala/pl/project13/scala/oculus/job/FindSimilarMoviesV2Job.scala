8ec5080b6b5fab80a657eb556bb5803d3fa29705
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-29 16:48:29 +0100

    fix

--- scalding/src/main/scala/pl/project13/scala/oculus/job/FindSimilarMoviesV2Job.scala
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/FindSimilarMoviesV2Job.scala
@@ -90,7 +90,7 @@ class FindSimilarMoviesV2Job(args: Args) extends Job(args)
     .debug
     .groupBy('secondFrame) {
         _.sortBy('distance).take(TopKForFrame)
-//      _.sortWithTake(('distance, 'idRef, 'secondRef) -> 'distanceForFrame, TopNForFrame) {
+//      _.sortWithTake(('distance, 'idRef, 'secondRef) -> 'distanceForFrame, TopKForFrame) {
 //        (t1: (Long, Any, Any), t2: (Long, Any, Any)) => t1._1 < t2._1
 //      }
     }
