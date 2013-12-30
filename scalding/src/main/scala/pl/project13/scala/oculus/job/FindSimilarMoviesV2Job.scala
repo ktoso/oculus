5448acfad726a1e2e08d4a28a35f2b81b4f1cf38
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-30 15:13:34 +0100

    fix

--- scalding/src/main/scala/pl/project13/scala/oculus/job/FindSimilarMoviesV2Job.scala
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/FindSimilarMoviesV2Job.scala
@@ -17,8 +17,9 @@ class FindSimilarMoviesV2Job(args: Args) extends Job(args)
   /** seq file with images */
   val inputId = args("id")
 
-  val outputDistances = "/oculus/similar-to-" + inputId + "-distances" + "-withself-5_5" + ".out"
-  val outputRanking   = "/oculus/similar-to-" + inputId + "-ranking"   + "-withself-5_5" + ".out"
+  val outputDistances              = "/oculus/similar-to-" + inputId + "-distances" + "-withself-5_5" + ".out"
+  val outputRanking                = "/oculus/similar-to-" + inputId + "-ranking"   + "-withself-5_5" + ".out"
+  val outputTopMostSimilarForFrame = "/oculus/similar-to-" + inputId + "-top"       + "-withself-5_5" + ".out"
 
   implicit val mode = Read
 
@@ -30,8 +31,7 @@ class FindSimilarMoviesV2Job(args: Args) extends Job(args)
       .rename('id -> 'idFrame)
       .mapTo('frame -> 'frameFrame) { sec: ImmutableBytesWritable => ibwToLong(sec) }
       .rename('hash -> 'hashFrame)
-      .debug
-      .limit(5)
+      .limit(100)
 
   val otherHashes =
     HashesTable
@@ -42,7 +42,7 @@ class FindSimilarMoviesV2Job(args: Args) extends Job(args)
       .mapTo('frame -> 'frameRef) { sec: ImmutableBytesWritable => ibwToLong(sec) }
       .rename('hash -> 'hashRef)
       .sample(5.0)
-      .limit(100)
+      .limit(200)
       .debug
 
 //  val inputHistograms =
@@ -73,15 +73,19 @@ class FindSimilarMoviesV2Job(args: Args) extends Job(args)
   val totalDistances = distances
     .map('hashRef   -> 'hashRef) { h: ImmutableBytesWritable => ibwToString(h) }
     .map('hashFrame -> 'hashFrame) { h: ImmutableBytesWritable => ibwToString(h) }
-//    .groupBy('frameFrame) {
-//      _.sortWithTake(('distance, 'idRef, 'frameRef) -> 'distanceForFrame, TopKForFrame) {
-//        (t1: (Int, Any, Any), t2: (Int, Any, Any)) => t1._1 < t2._1
-//      }
-//    }
-    .write(Csv(outputRanking, writeHeader = true, fields = ('distance, 'idFrame, 'idRef, 'frameFrame, 'frameRef, 'hashFrame, 'hashRef)))
+    .groupBy('frameFrame) {
+      _.sortWithTake(('distance, 'idRef, 'frameRef) -> 'distanceForFrame, TopKForFrame) {
+        (t1: (Int, Any, Any), t2: (Int, Any, Any)) => t1._1 < t2._1
+      }
+    }
+    .write(Csv(outputRanking, writeHeader = true, fields = ('distance,   'idFrame, 'frameFrame,   'idRef, 'frameRef,   'distanceForFrame)))
 
   val mostSimilarMovie = totalDistances
-    .groupBy('idRef) { _.size('countOfSimilarMovie) }
+    .groupBy('idRef) {
+      _.size('countOfSimilarMovie)
+       .sortWithTake('countOfSimilarMovie -> 'c, 2) { (c1: Int, c2: Int) => c1 < c2 }
+    }
+    .write(Csv(outputTopMostSimilarForFrame, writeHeader = true, fields = ('idRef, 'c)))
 
 }
 
