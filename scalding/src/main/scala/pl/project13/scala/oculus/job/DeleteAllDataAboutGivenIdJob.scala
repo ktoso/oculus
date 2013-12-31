f17ae08dbf094d00c962a146aabc3b6d4424219f
Konrad Malawski <konrad.malawski@project13.pl>
2013-12-31 15:39:14 +0100

    fix

--- scalding/src/main/scala/pl/project13/scala/oculus/job/DeleteAllDataAboutGivenIdJob.scala
+++ scalding/src/main/scala/pl/project13/scala/oculus/job/DeleteAllDataAboutGivenIdJob.scala
@@ -13,21 +13,7 @@ class DeleteAllDataAboutGivenIdJob(args: Args) extends Job(args)
   with HistogramsSource with HashesSource with MetadataSource
   with Histograms {
 
-  implicit val hbaseConf = {
-    val c = HBaseConfiguration.create()
-    c.set("fs.default.name", "hdfs://10.240.57.179:9000")
-    c.set("hbase.rootdir", "hdfs://10.240.57.179:9000/hbase")
-    c.set("hbase.cluster.distributed", "true")
-    c.set("hbase.zookeeper.quorum", "10.240.57.179")
-    c
-  }
-
-
   val inputId = args("id")
-
-//  val HashesHTable     = new HTable(hbaseConf, "hashes")
-//  val HistogramsHTable = new HTable(hbaseConf, "histograms")
-//  val MetadataHTable   = new HTable(hbaseConf, "metadata")
 
   implicit val mode = Read
 
