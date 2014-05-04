5cab1a6ff909c269816d09ebc029cfe3b13ee0c7
Konrad Malawski <konrad.malawski@project13.pl>
2014-05-04 21:44:58 +0200

    phash distances

---
+++ phash-distances.sh
@@ -0,0 +1,150 @@
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+red part   = (R,97)
+green part = (G,106)
+blue part  = (B,53)
+red part   = (R,96)
+green part = (G,106)
+blue part  = (B,54)
+h1Dominates = (G,106)
+h2Dominates = (G,106)
+====
+d1 = DominatingColors(List((G,372876), (B,365043), (R,183681)),G,G40-B39-R19)
+d2 = DominatingColors(List((G,373049), (B,364317), (R,184234)),G,G40-B39-R19)
+=========
+one = /tmp/Big_Buck_Bunny_mirror.png
+two = /tmp/Big_Buck_Bunny_normal.png
+[success] Total time: 3 s, completed May 4, 2014 9:34:17 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 2 Scala sources to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/Big_Buck_Bunny_mirror.png
+Exec: image_hashes /tmp/Big_Buck_Bunny_normal.png
+===========================================
+oneHash.dctHash = 54086765383280
+-------------------------------------------
+oneHash.mhHash = 458193aa7ca2e2f03e29065f29f01019f4b813f07b217f94775dcb91985cc0ef83656b981b575f4d44ad7848c5469d1ad81a496665c3e262070e424d8592231a1c8118fcf2f0ef63
+===========================================
+===========================================
+twoHash.mhHash = 8e2f6d04831568425b3c5c2ca01afd88b6ad638d43ced55d71cc032c53abdc898e930523e00fb334e765e57529f0ca5b6b6333a35076b1fd249a1e00e0e7c45beaa10792bc453228
+-------------------------------------------
+twoHash.dctHash = 54086765383280
+===========================================
+-------------------------------------------
+dctDistance = 0
+mhDistance = 402
+-------------------------------------------
+name1 = Big_Buck_Bunny_mirror.png
+name2 = Big_Buck_Bunny_normal.png
+[success] Total time: 4 s, completed May 4, 2014 9:35:41 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 1 Scala source to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/bunny-butterfly.png
+Exec: image_hashes /tmp/bunny-butterfly-5plus-light.png
+===========================================
+oneHash.dctHash = 4370297355906179638
+-------------------------------------------
+oneHash.mhHash = 36a943c937d34edd53b4242eb8d6dc77a4f9a4db1a6b2b264ed44a9584ee9a26aeaac34ad35aeb566bd1266d4ba2bcc9b32c969c59626ca6fb59b4527cdd68a34a93b8d315a73b38
+===========================================
+===========================================
+twoHash.mhHash = 36a94340b7d1497d13b6242e9852dc772471a4db1aeb2b260ef44a90846eba2eaeaac348995aeb526bd1266965a6bc49b32e869c596368a67a59b4507cb42cab6a13b8db13a6331a
+-------------------------------------------
+twoHash.dctHash = 4370297355906179638
+===========================================
+-------------------------------------------
+dctDistance = 0
+mhDistance = 132
+-------------------------------------------
+name1 = bunny-butterfly.png
+name2 = bunny-butterfly-5plus-light.png
+[success] Total time: 2 s, completed May 4, 2014 9:37:31 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 1 Scala source to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/bunny-standing.png
+Exec: image_hashes /tmp/bunny-streching.png
+===========================================
+oneHash.dctHash = 10819398131133106010
+-------------------------------------------
+oneHash.mhHash = bcb78b50d83d51d754b42511485965b0d3d357c17438eaadceda1fb40996ec9733448ff2a608ec7933438d904999499f7256c9747339c1abd449061ba9bc3fe73b5d52fc69684b26
+===========================================
+===========================================
+twoHash.mhHash = 98f0aa21588395d529e4a5b1a969350ad33961d2f379285b2c9a3d26898a64d577288dc26e4c9a44904a88d569d9689a648749708fe9838f0cc7225738f0bbb252e4b3c8a2487d13
+-------------------------------------------
+twoHash.dctHash = 12710979827094963034
+===========================================
+-------------------------------------------
+dctDistance = 31
+mhDistance = 324
+-------------------------------------------
+name1 = bunny-standing.png
+name2 = bunny-streching.png
+[success] Total time: 2 s, completed May 4, 2014 9:39:10 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 1 Scala source to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/image919.png
+Exec: image_hashes /tmp/bunny-streching.png
+===========================================
+oneHash.dctHash = 15191531720529365775
+-------------------------------------------
+oneHash.mhHash = 69b5598334394f15926d573388f0fb28619269b36744378a3859b64d3cd2bf06bdb55b366d8f74fda5799297b44d14b8bc59a8fcab264d9e1a9660b4e92db462aaea2fa0b4b7d53a
+===========================================
+===========================================
+twoHash.mhHash = 98f0aa21588395d529e4a5b1a969350ad33961d2f379285b2c9a3d26898a64d577288dc26e4c9a44904a88d569d9689a648749708fe9838f0cc7225738f0bbb252e4b3c8a2487d13
+-------------------------------------------
+twoHash.dctHash = 12710979827094963034
+===========================================
+-------------------------------------------
+dctDistance = 35
+mhDistance = 369
+-------------------------------------------
+name1 = image919.png
+name2 = bunny-streching.png
+[success] Total time: 2 s, completed May 4, 2014 9:40:03 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 1 Scala source to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/Big_Buck_Bunny_mirror.png
+Exec: image_hashes /tmp/bunny-streching.png
+===========================================
+oneHash.dctHash = 54086765383280
+-------------------------------------------
+oneHash.mhHash = 458193aa7ca2e2f03e29065f29f01019f4b813f07b217f94775dcb91985cc0ef83656b981b575f4d44ad7848c5469d1ad81a496665c3e262070e424d8592231a1c8118fcf2f0ef63
+===========================================
+===========================================
+twoHash.mhHash = 98f0aa21588395d529e4a5b1a969350ad33961d2f379285b2c9a3d26898a64d577288dc26e4c9a44904a88d569d9689a648749708fe9838f0cc7225738f0bbb252e4b3c8a2487d13
+-------------------------------------------
+twoHash.dctHash = 12710979827094963034
+===========================================
+-------------------------------------------
+dctDistance = 25
+mhDistance = 355
+-------------------------------------------
+name1 = Big_Buck_Bunny_mirror.png
+name2 = bunny-streching.png
+[success] Total time: 2 s, completed May 4, 2014 9:40:31 PM
+> run-main pl.project13.scala.oculus.HashDistancesApp
+[info] Compiling 1 Scala source to /home/ktoso/code/oculus/scalding/target/scala-2.10/classes...
+[info] Running pl.project13.scala.oculus.HashDistancesApp 
+Exec: image_hashes /tmp/Big_Buck_Bunny_mirror.png
+Exec: image_hashes /tmp/bunny-standing.png
+===========================================
+oneHash.dctHash = 54086765383280
+-------------------------------------------
+oneHash.mhHash = 458193aa7ca2e2f03e29065f29f01019f4b813f07b217f94775dcb91985cc0ef83656b981b575f4d44ad7848c5469d1ad81a496665c3e262070e424d8592231a1c8118fcf2f0ef63
+===========================================
+===========================================
+twoHash.mhHash = bcb78b50d83d51d754b42511485965b0d3d357c17438eaadceda1fb40996ec9733448ff2a608ec7933438d904999499f7256c9747339c1abd449061ba9bc3fe73b5d52fc69684b26
+-------------------------------------------
+twoHash.dctHash = 10819398131133106010
+===========================================
+-------------------------------------------
+dctDistance = 29
+mhDistance = 361
+-------------------------------------------
+name1 = Big_Buck_Bunny_mirror.png
+name2 = bunny-standing.png
+[success] Total time: 2 s, completed May 4, 2014 9:41:00 PM
+> 
+
